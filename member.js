960ebab478994ff51e7d487f117dbf16ec54a88f
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2024-03-17 15:52:13 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember()
+{
+    var member = {
+        name: "John",
+        age: 30,
+        skills: ["JavaScript", "HTML", "CSS"],
+        address: {
+            city: "New York",
+            country: "USA"
+        }
+    };
+    return member;
+}
